47d36d2f7e79bd70b4883d4692957ae19fa9bf87
orioncrew <orioncrew@yandex.ru>
2018-12-05 22:30:45 +0500

    конструктор

---
+++ snake/Point.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace snake
+{
+    public class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+
+        public Point()
+        {
+        }
+    }
+}
--- snake/Program.cs
+++ snake/Program.cs
@@ -6,7 +6,15 @@ namespace snake
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Point p1 = new Point(1, 3, '*');
+            p1.Draw();
+
+            Point p2 = new Point(4, 5, '#');
+            p2.Draw();
+
+
         }
+
+
     }
 }
